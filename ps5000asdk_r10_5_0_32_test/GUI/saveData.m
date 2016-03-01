82c2a19f0efe143594a2e57701ae30df6a1334f6
Alex Benjamin <Alex Benjamin>
2016-03-01 16:50:02 -0500

    Added some exception handling to transducerStatusGroupSelectionChanged.m

--- ps5000asdk_r10_5_0_32_test/GUI/saveData.m
+++ ps5000asdk_r10_5_0_32_test/GUI/saveData.m
@@ -1,12 +1,14 @@
 if strcmp(currentStatus(2), status.transducerNotCollectingData)
     [filename, pathname] = uiputfile('*.mat', 'Save scan data...');
     if filename
+        fprintf('Saving data...\n');
         if collected1DData
             [scopeStatus.time, scopeStatus.numSamples, scopeStatus.buffers] = setNumCaptures(100);
             [collectedData, elapseTime] = runScope1Ch();
             [scopeStatus.time, scopeStatus.numSamples, scopeStatus.buffers] = setNumCaptures(2);
         end
         save(strcat(pathname, filename), 'collectedData');
+        fprintf('Done saving data.\n');
     end
 else
     errordlg('You must pause data collection before saving any data.', 'Cannot save');
--- ps5000asdk_r10_5_0_32_test/GUI/transducerStatusGroupSelectionChanged.m
+++ ps5000asdk_r10_5_0_32_test/GUI/transducerStatusGroupSelectionChanged.m
@@ -5,13 +5,16 @@ switch selectedOption
         %set(ps5000aDeviceObj, 'numCaptures', 2);
         currentStatus(2) = {status.transducerCollecting1DScanData};
         setCurrentStatus;
-        while strcmp(currentStatus(2), status.transducerCollecting1DScanData)
-            [scanningData, elapseTime] = runScope1Ch();
-            plot(sum(scanningData)./size(scanningData,1))
-            ylabel('Voltage (V)');
-            xlabel('Data Number');
-            title('Pulse-Echo Response');
-            pause(0.005);
+        try
+            while strcmp(currentStatus(2), status.transducerCollecting1DScanData)
+                [scanningData, elapseTime] = runScope1Ch();
+                plot(sum(scanningData)./size(scanningData,1))
+                ylabel('Voltage (V)');
+                xlabel('Data Number');
+                title('Pulse-Echo Response');
+                pause(0.005);
+            end
+        catch ex
         end
         collectedData = scanningData;
         collected1DData = 1;
@@ -19,17 +22,20 @@ switch selectedOption
         %set(ps5000aDeviceObj, 'numCaptures', 100);
         currentStatus(2) = {status.transducerCollecting2DScanData};
         setCurrentStatus;
-        i = 1;
-        while strcmp(currentStatus(2), status.transducerCollecting2DScanData)
-            fprintf('Capture Number %d\n', i);
-            [dataA, elapseTime] = runScope1Ch();
-            size(dataA)
-            imagesc(abs(dataA(1:i,:)'), [-0.2 0.2]);
-            xlabel('Scan Number');
-            ylabel('Sample Number');
-            title('Image Construction');
-            pause(0.05);
-            i = i + 1;
+        try
+            i = 1;
+            while strcmp(currentStatus(2), status.transducerCollecting2DScanData)
+                fprintf('Capture Number %d\n', i);
+                [dataA, elapseTime] = runScope1Ch();
+                size(dataA)
+                imagesc(abs(dataA(1:i,:)'), [-0.2 0.2]);
+                xlabel('Scan Number');
+                ylabel('Sample Number');
+                title('Image Construction');
+                pause(0.05);
+                i = i + 1;
+            end
+        catch ex
         end
         collectedData = dataA;
         collected1DData = 0;
